f431816c30c905dcd734f8e08714d760cfdcf9dd
Aiman Afzal <aimiafzal.11@gmail.com>
2026-05-01 22:53:50 +0500

    Update auction.h

--- auction.h
+++ auction.h
@@ -9,22 +9,20 @@ private:
 	string itemName;
 	bool isActive;
 public:
-	//Function overloading
-	//1. Default initialization
 	void placeBid() {
 		bidderName = "Unknown";
 		bidAmount = 0.0;
 		itemName = "Unknown";
 		isActive = true;
 	}
-	//2. Parameterized initialization with only name and price
+
 	void placeBid(string name, double amt) {
 		bidderName = name;
 		bidAmount = amt;
 		itemName = "General item";
 		isActive = true;
 	}
-	//3. Parameterized initialization with all details
+
 	void placeBid(string name, double amt, string item) {
 		bidderName = name;
 		bidAmount = amt;
@@ -32,7 +30,7 @@ public:
 		isActive = true;
 	}
 
-	//getter functions 
+
 	string get_name() {
 		return bidderName;
 	}
@@ -40,7 +38,6 @@ public:
 		return bidAmount;
 	}
 
-	//Constructors
 	Bid() {
 		placeBid();
 	}
@@ -51,18 +48,14 @@ public:
 		placeBid(name, amt, item);
 	}
 
-	//increase bid amount
 	void raiseBid(double raise) {
 		bidAmount += raise;
 	}
 
-	//cancel bid
 	void cancelBid() {
 		isActive = false;
 	}
 
-	//Operator overloading
-	//1. +operator (2 joined bids e.g group bid)
 	Bid operator+(const Bid &other) {
 		Bid result;
 		result.bidderName = bidderName + " & " + other.bidderName,
@@ -71,7 +64,6 @@ public:
 		return result;
 	}
 
-	//2. -operator (decreasing the bid price)
 	Bid operator-(double deduction)const {
 		Bid result = *this;
 		if (bidAmount - deduction > 0)
@@ -81,17 +73,14 @@ public:
 		return result;
 	}
 
-	//3. >operator (to evaluate higher bid)
 	bool operator>(const Bid& other) {
 		return (bidAmount > other.bidAmount);
 	}
 
-	//4. ==operator (to check if two bids are of equal amount)
 	bool operator==(const Bid& other) const{
 		return bidAmount == other.bidAmount;
 	}
 
-	//5. <<operator (for displaying all details)
 	friend ostream& operator<<(ostream& out, const Bid& b) {
 		out << "------------------------------\n";
 		out << "Bidder : " << b.bidderName << endl;
